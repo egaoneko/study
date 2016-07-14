936c7689a0aa4e2ca479b45c3327779f897c61a8
donghyun seo <donghyun.seo@ticketlink.co.kr>
2016-07-14 09:32:22 +0900

    Edit angularjs splitter

--- angular/splitter/app.js
+++ angular/splitter/app.js
@@ -2,7 +2,7 @@
 
 var app = angular.module('sampleApp', ['ui.layout']);
 
-app.directive('sdiv', function () {
+app.directive('pdiv', function ($timeout) {
     return {
         template:'<div style="background : #fff; width: {{width}}; height: {{height}}; margin : 10px; display:inline-block;"><span>x: {{x}}, <br>y: {{y}}</span></div>',
         restrict:"AE",
@@ -11,24 +11,32 @@ app.directive('sdiv', function () {
             height: "@"
         },
         link: function (scope, element, attrs) {
-            scope.x = offset(element).left;
-            scope.y = offset(element).top;
+            $timeout(function(){
+                var offsetXY = offset(element);
+                scope.x = offsetXY.left;
+                scope.y = offsetXY.top;
+            });
+
+            element.on('click', function () {
+                console.log(offset(element));
+            })
 
             function offset(element) {
-              var rawDomNode = element[0];
-              var body = document.documentElement || document.body;
-              var scrollX = window.pageXOffset || body.scrollLeft;
-              var scrollY = window.pageYOffset || body.scrollTop;
-              var clientRect = rawDomNode.getBoundingClientRect();
-              var x = clientRect.left + scrollX;
-              var y = clientRect.top + scrollY;
-              return { left: parseInt(x), top: parseInt(y) };
+                var rawDomNode = element[0];
+                var clientRect = rawDomNode.getBoundingClientRect();
+
+                var body = document.documentElement || document.body;
+                var scrollX = window.pageXOffset || body.scrollLeft;
+                var scrollY = window.pageYOffset || body.scrollTop;
+                var x = clientRect.left + scrollX;
+                var y = clientRect.top + scrollY;
+                return { left: parseInt(x), top: parseInt(y) };
             }
         }
     }
 })
 
-app.controller("TestCtrl", ['$scope', function ($scope) {
+app.controller("UtilCtrl", ['$scope', function ($scope) {
     $scope.range = function(n) {
         return new Array(n);
     };
