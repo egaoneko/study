b9855ebcd951340dacf8f868a8eb46e77272fccd
Donghyun <egaoneko@naver.com>
2017-03-12 00:18:14 +0900

    [react tutorial]

--- react-tutorial/src/components/App.js
+++ react-tutorial/src/components/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
 
 class App extends React.Component {
-
-    sayHey() {
-        alert("hey");
-    }
-
     render() {
-        let text = "Dev-Server"
-
-        let pStyle = {
-            color: 'aqua',
-            backgroundColor: 'black'
-        };
-
         return(
             <div>
-                <h1>Hello React Skeleton</h1>
-                <h2> Welcome to {text}</h2>
-                <button onClick={this.sayHey}>Click Me</button>
-                <p style = {pStyle}>{1 == 1? 'True' : 'False'}</p>
+                <Header/>
+                <Content/>
             </div>
         );
     }
