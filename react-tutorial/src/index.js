d90590476ad86b1a0587333ab52e2dccc9eb16ab
Donghyun <egaoneko@naver.com>
2017-03-14 01:24:16 +0900

    [react tutorial]

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import App2 from './components/App2';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+// ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App2 />, rootElement);
