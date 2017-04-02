8568bb6445c77dd1c85ad166aa34978a9955465f
Donghyun <egaoneko@naver.com>
2017-04-02 21:36:09 +0900

    [CTM]

--- coding_the_matrix/ch8/task.py
+++ coding_the_matrix/ch8/task.py
@@ -4,13 +4,19 @@ from ch5.resource.matutil import *
 from ch3.resource.vecutil import *
 from ch5.resource.bitutil import *
 
-rowlist = [list2vec(v) for v in [[0, 2, 3, 4, 5], [0, 0, 0, 3, 2], [1, 2, 3, 4, 5], [0, 0, 0, 6, 7], [0, 0, 0, 9, 9]]]
+rowlist = [list2vec(v) for v in [[0, 2, 3, 4, 5], [0, 0, 0, 3, 2], [1, 2, 3, 4, 5], [0, 0, 0, 6, 7], [0, 0, 0, 9, 8]]]
 print("rowlist: ", rowlist)
 
+row_labels = rowlist[0].D
 col_label_list = sorted(rowlist[0].D, key=hash)
+row_label_list = sorted(rowlist[0].D, key=hash)
 print("col_label_list: ", col_label_list)
 
+M_rowlist = [Vec(row_labels, {row_label_list[i]: 1}) for i in range(len(col_label_list))]
+print("M_rowlist: ", M_rowlist)
+
 new_rowlist = []
+new_M_rowlist = []
 rows_left = set(range(len(rowlist)))
 print("rows_left: ", rows_left)
 
@@ -25,6 +31,9 @@ for c in col_label_list:
         new_rowlist.append(rowlist[pivot])
         print("new_rowlist: ", new_rowlist)
 
+        new_M_rowlist.append(M_rowlist[pivot])
+        print("new_M_rowlist.: ", new_M_rowlist)
+
         rows_left.remove(pivot)
         print("rows_left: ", rows_left)
 
@@ -33,10 +42,19 @@ for c in col_label_list:
             print("multiplier: ", multiplier)
 
             rowlist[r] -= multiplier * rowlist[pivot]
+            M_rowlist[r] -= multiplier * M_rowlist[pivot]
 
-        print("rows_left: ", rows_left)
+for r in rows_left:
+    new_rowlist.append(rowlist[r])
 
-print("rowdict2mat: ", rowdict2mat(new_rowlist))
+for r in rows_left:
+    new_M_rowlist.append(M_rowlist[r])
+
+    print("rows_left: ", rows_left)
+
+print("rowdict2mat new_rowlist: ", rowdict2mat(new_rowlist))
+print("rowdict2mat M_rowlist: ", rowdict2mat(M_rowlist))
+print("rowdict2mat new_M_rowlist: ", rowdict2mat(new_M_rowlist))
 
 M = listlist2mat([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, -2, 1, 0], [0, 0, 0, 0, 1]])
 A = listlist2mat([[0, 2, 3, 4, 5], [0, 0, 0, 3, 2], [1, 2, 3, 4, 5], [0, 0, 0, 6, 7], [0, 0, 0, 9, 8]])
