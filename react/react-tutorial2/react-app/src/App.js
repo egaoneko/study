be71b265e6f2c6dc4598b759828ba91316d30d00
donghyun seo <donghyun.seo@ticketlink.co.kr>
2017-05-26 19:31:13 +0900

    [react tutorial]

--- react/react-tutorial2/react-app/src/App.js
+++ react/react-tutorial2/react-app/src/App.js
@@ -6,12 +6,22 @@ class App extends Component {
     //     <h1> Hello World</h1> <b>Bold</b>
     // )
     return (
+        let txt = this.props.txt;
         <div>
-            <h1>Hello World</h1>
+            <h1>{txt}</h1>
             <b>Bold</b>
         </div>
     );
   }
+}
+
+App.propTypes = {
+    txt: React.propTypes.string,
+    cat: React.propTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: "this is the default text"
 }
 
 // const App = () => <h1>Hello World</h1>
