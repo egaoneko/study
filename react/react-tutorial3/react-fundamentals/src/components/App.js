99189f68ebc851e6ce0c2b4c7018e2c37e2fadd6
Donghyun <egaoneko@naver.com>
2017-07-06 00:54:04 +0900

    [react tutorial]

--- react/react-tutorial3/react-fundamentals/src/components/App.js
+++ react/react-tutorial3/react-fundamentals/src/components/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: ''
-        };
-    }
     render() {
         return (
             <div>
-                <button onClick={()=> {this.setState({name: 'Velopert'})}}>Click</button>
-                <h1>Hello!!! {this.state.name}</h1>
+                <Contact/>
             </div>
         );
     }
