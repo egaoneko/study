42e3d622e2ed245ca6ae1510837b611a66524209
Donghyun <egaoneko@naver.com>
2017-07-06 22:30:27 +0900

    [react tutorial3]

--- react/react-tutorial3/react-fundamentals/src/components/Contact.js
+++ react/react-tutorial3/react-fundamentals/src/components/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
 
 export default class Contact extends React.Component {
     
     constructor(props) {
         super(props);
         this.state = {
+            selectedKey: -1,
             keyword: '',
             contactData: [{
                 name: 'Abet',
@@ -23,12 +25,20 @@ export default class Contact extends React.Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(e) {
         this.setState({
             keyword: e.target.value
         });
+    }
+
+    handleClick(key) {
+        this.setState({
+            selectedKey: key
+        });
+        console.log(key);
     }
     
     render() {
@@ -44,7 +54,10 @@ export default class Contact extends React.Component {
                 return name.indexOf(keyword) > -1;
             })
             return data.map((contact, i) => {
-                return (<ContactInfo contact={contact} key={i}/>);
+                return (<ContactInfo 
+                                    contact={contact} 
+                                    key={i}
+                                    onClick={()=>{this.handleClick(i)}}/>);
             });
         };
         
@@ -57,6 +70,9 @@ export default class Contact extends React.Component {
                     value={this.state.keyword}
                     onChange={this.handleChange}/>
                 <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails 
+                    isSelected={this.state.selectedKey != -1}
+                    contact={this.state.contactData[this.state.selectedKey]}/>
             </div>
         );
     }
