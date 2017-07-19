8e930b63bfbba6c8df8fc14f8b9e3dc637adc71e
Donghyun <egaoneko@naver.com>
2017-07-20 00:22:26 +0900

    [redux-example]

--- react/redux-example/src/index.js
+++ react/redux-example/src/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import reducers from './reducers';
+// import * as actions from './actions';
+
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers);
+
+// console.log(store.getState());
+// const unsubscribe = store.subscribe(() => console.log(store.getState()));
+// store.dispatch(actions.increment());
+// store.dispatch(actions.increment());
+// store.dispatch(actions.decrement());
+// store.dispatch(actions.setColor([200, 200, 200]));
+// unsubscribe();
+// store.dispatch(actions.setColor([255, 255, 255]));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
